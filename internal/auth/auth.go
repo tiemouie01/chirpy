f5241ab7eb10e4aba26861e247e160a553c099f6
Timothy Miamba <timal0361@gmail.com>
2024-10-20 19:24:40 +0200

    feat: Add auth package with password hashing

---
+++ internal/auth/auth.go
@@ -0,0 +1,23 @@
+package auth
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		return "", errors.New(err.Error())
+	}
+	return string(hashedPassword), nil
+}
+
+func CheckPasswordHash(password, hash string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return errors.New(err.Error())
+	}
+	return nil
+}
