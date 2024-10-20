c3c1e71a28d61b690d3e368dcc6629dfccd331bd
Timothy Miamba <timal0361@gmail.com>
2024-10-20 16:23:36 +0200

    chore: Update users table to include hashed password field

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,8 +3,19 @@ CREATE TABLE "users" (
     id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    email TEXT UNIQUE NOT NULL
+    email TEXT UNIQUE NOT NULL,
+    hashed_password TEXT DEFAULT 'unset'
+);
+
+CREATE TABLE chirps (
+    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    body TEXT NOT NULL,
+    user_id uuid,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- +goose Down
 DROP TABLE users;
+DROP TABLE chirps;
