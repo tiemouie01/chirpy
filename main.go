4393c3f8ba68eef4fe71cdef58372f99500aa2a1
Timothy Miamba <timal0361@gmail.com>
2024-11-01 21:20:43 +0200

    chore: Add JWT secret to api config

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 	dbQueries      *database.Queries
 	platform       string
+	jwtSecret      string
 }
 
 type jsonError struct {
@@ -37,10 +38,14 @@ func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	// Add JWT secret to api config
+	jwtSecret := os.Getenv("JWT_SECRET")
+
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 		dbQueries:      dbQueries,
 		platform:       os.Getenv("PLATFORM"),
+		jwtSecret:      jwtSecret,
 	}
 
 	mux := http.NewServeMux()
